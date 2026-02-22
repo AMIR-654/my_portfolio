fef5f9b8b9ba1e54784da995747aa6f8f6d9a661
AMIR <amerabualnagah@gmail.com>
2026-02-22 23:52:59 +0200

    Fix API_BASE_URL to use correct HTTPS endpoint
    Updated API_BASE_URL to use a valid HTTPS link.

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -4,7 +4,5 @@ import { projectId, publicAnonKey } from '/utils/supabase/info';
 const supabaseUrl = `https://${projectId}.supabase.co`;
 export const supabase = createClient(supabaseUrl, publicAnonKey);
 
-// export const API_BASE_URL = `${supabaseUrl}/functions/v1/make-server-cf19bf36`;
-
-
-export const API_BASE_URL = "backend-production-50ba.up.railway.app"
+// export const API_BASE_URL = 
+export const API_BASE_URL = "https://50ba.up.railway.app";
