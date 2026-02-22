fdc6cbdb138e93d0ae41604060f7084cf4ff2de9
AMIR <amerabualnagah@gmail.com>
2026-02-22 22:45:38 +0200

    Update supabase.ts

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -7,4 +7,4 @@ export const supabase = createClient(supabaseUrl, publicAnonKey);
 // export const API_BASE_URL = `${supabaseUrl}/functions/v1/make-server-cf19bf36`;
 
 
-export const API_BASE_URL = "backend-production-2ab7.up.railway.app"
+export const API_BASE_URL = "backend-production-37cb.up.railway.app"
